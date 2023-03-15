cd3a63fa004938a56316b7171052be41b02a7d11
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-03-15 20:44:59 +0530

    Candy Distribution

---
+++ Candy_Distribution.cpp
@@ -0,0 +1,24 @@
+/*  Chef has N candies. He has to distribute them to exactly M of his friends such that each friend gets equal number of candies and
+    each friend gets even number of candies. Determine whether it is possible to do so.
+    NOTE: Chef will not take any candies himself and will distribute all the candies. 
+  */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    if((n/m)%2==0 && n%m==0){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
