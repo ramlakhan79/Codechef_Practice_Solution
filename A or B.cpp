bcc70c928b547aa6e40e64998cb656dc8e643290
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-04-28 21:51:06 +0530

    Create A or B.cpp

---
+++ A or B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int ab,ba;
+	    
+	    ab=(500-x*2)+(1000-(x+y)*4);
+	    ba=(1000-y*4)+(500-(x+y)*2);
+	    
+	    if(ab>ba)
+	        cout<<ab<<endl;
+	    else
+	        cout<<ba<<endl;
+	}
+	return 0;
+}
