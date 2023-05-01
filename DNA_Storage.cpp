604c074be2af7f62171e1cd61f7a25990515945f
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-05-01 22:09:53 +0530

    Create DNA_Storage.cpp

---
+++ DNA_Storage.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    string s,ch;
+	    cin>>s;
+	    for(int i=0;i<n;i=i+2){
+	        if(s[i]=='0' && s[i+1]=='0'){
+	            ch=ch+'A';
+	        }
+	        else if(s[i]=='0' && s[i+1]=='1'){
+	            ch=ch+'T';
+	        }
+	        else if(s[i]=='1' && s[i+1]=='0'){
+	            ch=ch+'C';	  
+	        }
+	        else if(s[i]=='1' && s[i+1]=='1'){
+	            ch=ch+'G';	            
+	        }
+	    }
+	    cout<<ch<<endl;
+	    
+	}
+	return 0;
+}
