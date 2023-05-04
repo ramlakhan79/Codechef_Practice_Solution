d2b57a949283511a200e97ea5506dc4e4fa3a4b2
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-05-04 21:58:24 +0530

    Create Building_Race.cpp

---
+++ Building_Race.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    double a,b,x,y;
+	    cin>>a>>b>>x>>y;
+	    if(a/(x*1)>b/(y*1)){
+	        cout<<"Chefina"<<endl;
+	    }
+	    else if(a/(x*1)<b/(y*1)){
+	        cout<<"Chef"<<endl;
+	    }
+	    else{
+	        cout<<"Both"<<endl;
+	    }
+	}
+	return 0;
+}
