925a0f509f860fc779c5345e80b9d4bad44ec342
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-05-08 22:06:23 +0530

    Create Broken_Telephone.cpp

---
+++ Broken_Telephone.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t, n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && i < n - 1)
+            {
+                if (a[i] != a[i - 1] || a[i] != a[i + 1])
+                {
+                    count++;
+                }
+            }
+        }
+        if (a[n - 1] != a[n - 2])
+        {
+            count++;
+        }
+        if (a[0] != a[1])
+        {
+            count++;
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
