cc6c17d6998680139101304a4ee039caa3e51d2a
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-05-25 23:42:41 +0530

    Create Single_use_Attack.cpp

---
+++ Single_use_Attack.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	std::cin >> t;
+	while(t--)
+	{
+	    int h,x,y;
+	    std::cin >> h>>x>>y;
+	    int attack=0;
+	    h=h-y;
+	    attack++;
+	    
+	    while(h>0)
+	    {
+	        h=h-x;
+	        attack++;
+	    }
+	    
+	    
+	    std::cout << attack << std::endl;
+	    
+	}
+	
+	return 0;
+}
