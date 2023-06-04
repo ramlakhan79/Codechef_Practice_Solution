4d654b129e46ed8a948728d83a2e0860426b192f
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-06-04 10:09:52 +0530

    Create Paper_Cuting.cpp

---
+++ Paper_Cuting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    cout<<pow((n/k),2)<<endl;
+	}
+	return 0;
+}
