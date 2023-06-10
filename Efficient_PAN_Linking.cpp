3e685ab310036d736be41c39ab2cbf9cdd63beb0
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-06-10 10:46:56 +0530

    Create Efficient_PAN_Linking.cpp

---
+++ Efficient_PAN_Linking.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include<boost/multiprecision/cpp_int.hpp>
+#define ll long long
+using namespace boost::multiprecision;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	   int1024_t n;
+        	    
+	    cin>>n;
+	    cout<<n%20<<endl;
+	}
+	return 0;
+}
