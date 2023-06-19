0a5bb80889bf5f72e91d868fa99a6942e20ab5c3
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-06-19 21:45:27 +0530

    Create Food_Chain.cpp

---
+++ Food_Chain.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() 
+{
+	ll T;
+	cin>>T;
+	while(T--)
+	{
+	    ll a,b,c=0;
+	    cin>>a>>b;
+	    while(a/b)
+	    {
+	        c++;
+	        a=a/b;
+	    }
+	    cout<<c+1<<endl;
+	}
+	return 0;
+}
