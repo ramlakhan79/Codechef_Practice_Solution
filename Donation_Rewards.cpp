4f659fc0e6372d34f9793c688f7f32dfed49776f
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-06-21 22:00:25 +0530

    Create Donation_Rewards.cpp

---
+++ Donation_Rewards.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x<=3){
+	        cout<<"BRONZE"<<endl;
+	    }
+	    else if(x>3 && x<=6){
+	        cout<<"SILVER"<<endl;
+	    }
+	    else if(x>6){
+	        cout<<"GOLD"<<endl;
+	    }
+	}
+	return 0;
+}
