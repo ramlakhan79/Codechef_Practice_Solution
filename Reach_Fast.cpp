187d91d8426cf74bad834ee72fe0e53b80843eab
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-06-27 15:51:27 +0530

    Create Reach_Fast.cpp

---
+++ Reach_Fast.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x,y,k,res;
+	    cin>>x>>y>>k;
+	    
+	    if(x>=y){
+	        if((x-y)%k==0)
+	        res=(x-y)/k;
+	        else
+	        res=((x-y)/k)+1;
+	    }
+	    else{
+	       if((y-x)%k==0)
+	       res=(y-x)/k;
+	       else
+	       res=((y-x)/k)+1;
+	}
+	
+		cout<<res<<endl;
+	
+}
+	return 0;
+}
