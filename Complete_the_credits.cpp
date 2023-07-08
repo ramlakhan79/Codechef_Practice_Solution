b2729a6cadb1d7c8166b14c9fcadc1169474d9b0
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-07-08 11:29:17 +0530

    Create Complete_the_credits.cpp

---
+++ Complete_the_credits.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>65){
+	        cout<<"Overload"<<endl;
+	    }
+	    else if(x<35){
+	        cout<<"Underload"<<endl;
+	    }
+	    else{
+	        cout<<"Normal"<<endl;
+	    }
+	}
+	return 0;
+}
