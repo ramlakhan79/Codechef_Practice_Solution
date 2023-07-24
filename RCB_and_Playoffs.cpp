9240a8ce8210dd45913c69c27f3b2f54e1268d7c
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-07-24 20:50:00 +0530

    Create RCB_and_Playoffs.cpp

---
+++ RCB_and_Playoffs.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    if(y<=x+z*2)
+	        cout<<"YES"<<endl;
+	        
+	    else
+	        cout<<"NO"<<endl;
+	}
+	return 0;
+}
