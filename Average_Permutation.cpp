f7b1d2ec3734495c66099c78a6e2f0e1e65ad260
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-08-14 23:26:57 +0530

    Average_Permutation.cpp

--- Average_Permutation.cpp
+++ Average_Permutation.cpp
@@ -10,6 +10,7 @@ int main() {
         vector<int> v;
         v.push_back(n - 1);
         v.push_back(n - 2);
+        
         for (int i = 1; i <= (n - 3); i++) {
             v.push_back(i);
         }
