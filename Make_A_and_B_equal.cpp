b27bf6e27b45662d30ab8f2bb46d0c7921af893a
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-08-31 12:18:54 +0530

    Create Make_A_and_B_equal.cpp

---
+++ Make_A_and_B_equal.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a==b){
+	        cout<<"YES"<<endl;
+	    }
+	   else if (a<b)
+	    {
+	        while(a<b)
+	        {
+	            a=2*a;
+	           
+	        }if (a==b)
+	        cout<<"YES"<<endl;
+	        else
+	        cout<<"NO"<<endl;
+	    }
+	    else if (a>b)
+	    {
+	        while(b<a)
+	        {
+	            b=2*b;
+	        }if (a==b)
+	        cout<<"YES"<<endl;
+	        else
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
