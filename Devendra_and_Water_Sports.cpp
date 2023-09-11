748ddc51278726c4105cd097685416447c6a5ab4
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-09-11 19:03:27 +0530

    Create Devendra_and_Water_Sports.cpp

---
+++ Devendra_and_Water_Sports.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int z,y,a,b,c;
+	    cin>>z>>y>>a>>b>>c;
+	    if((z-y)>=(a+b+c)){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
