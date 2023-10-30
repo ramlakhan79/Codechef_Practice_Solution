4459f785657f56d25298317d83010d5dbd2114b4
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-10-30 23:34:37 +0530

    Create Chef_and_Game_with_sequence.cpp

---
+++ Chef_and_Game_with_sequence.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    int even_no=0,odd_no=0;
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	        if(arr[i]%2==0){
+	            even_no++;
+	        }
+	        else{
+	            odd_no++;
+	        }
+	    }
+	   
+	    
+	    if(odd_no%2==0 || n==1)
+	        cout<<1<<endl;
+	    else
+	        cout<<2<<endl;
+	    
+	}
+	return 0;
+}
