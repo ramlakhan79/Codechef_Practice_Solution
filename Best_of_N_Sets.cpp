bb4ec34a387f5ea591279e31b7732db721d606ec
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-11-22 22:25:54 +0530

    Create Best_of_N_Sets.cpp

---
+++ Best_of_N_Sets.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>y){
+	        cout<<(x*2)-1<<endl;
+	    }
+	    else{
+	        cout<<(y*2)-1<<endl;
+	    }
+	}
+	return 0;
+}
