2d7a9f82afb3ff3802245524d868134308c47269
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-11-29 23:10:22 +0530

    Create Find_the_Direction.cpp

---
+++ Find_the_Direction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x%4==0){
+	        cout<<"North"<<endl;
+	    }
+	    else if(x%4==1){
+	        cout<<"East"<<endl;
+	    }
+	    else if(x%4==2){
+	        cout<<"South"<<endl;
+	    }
+	    else if(x%4==3){
+	        cout<<"West"<<endl;
+	    }
+	}
+	return 0;
+}
