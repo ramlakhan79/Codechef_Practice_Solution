e61e641a94744a73a0149cf18a3353ca268cde9e
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-12-03 17:14:34 +0530

    Create Airlines.cpp

---
+++ Airlines.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,n;
+	    cin>>x>>n;
+	    if((n%100==0 && x>=n/100) || (n%100!=0 && x>=(n/100)+1)){
+	        cout<<"0"<<endl;
+	    }
+	    else if(n%100==0){
+	        cout<<(n/100)-x<<endl;
+	    }
+	    else{
+	        cout<<((n/100)-x)+1<<endl;
+	    }
+	}
+	return 0;
+}
