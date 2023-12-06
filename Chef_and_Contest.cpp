2e16a94baaf451eb2ef4ab0766a01c829f43d92a
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-12-06 17:45:10 +0530

    Create Chef_and_Contest.cpp

---
+++ Chef_and_Contest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,p,q;
+	    cin>>x>>y>>p>>q;
+	    if((x+p*10)<(y+q*10)){
+	        cout<<"Chef"<<endl;
+	    }
+	    else if((x+p*10)>(y+q*10)){
+	        cout<<"Chefina"<<endl;
+	    }
+	    else{
+	        cout<<"Draw"<<endl;
+	    }
+	}
+	return 0;
+}
