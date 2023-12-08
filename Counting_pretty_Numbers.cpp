a9554709a13739fa1eb12d75556e43522a841c85
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-12-08 13:48:17 +0530

    Create Counting_pretty_Numbers.cpp

---
+++ Counting_pretty_Numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool checkDigit(int n){
+    if(n%10==2 || n%10==3 || n%10==9){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int l,r;
+	    cin>>l>>r;
+	    int count=0;
+	    for(int i=l;i<=r;i++){
+	        if(checkDigit(i))
+	            count++;
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
