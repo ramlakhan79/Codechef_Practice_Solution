9a257128fa6c63f1e8eda44183fc44354b35d035
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-12-09 13:58:49 +0530

    Create Drunk_Alcoholic.cpp

---
+++ Drunk_Alcoholic.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    int ans=0;
+	    for(int i=1;i<=k;i++){
+	        if(i%2!=0){
+	            ans+=3;
+	        }
+	        else{
+	            ans-=1;
+	        }
+	    }
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
