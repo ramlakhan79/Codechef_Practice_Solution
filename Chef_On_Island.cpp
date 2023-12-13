3ffa8860c21f608913b96c00cc1958e5c16a3b62
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-12-13 15:59:45 +0530

    Create Chef_On_Island.cpp

---
+++ Chef_On_Island.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    float x,y,xr,yr,d;
+	    cin>>x>>y>>xr>>yr>>d;
+	    if(min((x/xr),(y/yr))>=d){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
