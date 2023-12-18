02d4c7c95c82d74336cfc9227604fe78703061d4
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-12-18 23:33:42 +0530

    Create Different_Consecutive_Characters.cpp

---
+++ Different_Consecutive_Characters.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while (t--){
+	    int n;
+	    cin>>n;
+	    string s;
+	    cin>>s;
+	    int count=0;
+	    for (int i=1;i<n;i++){
+	        if(s[i-1]==s[i]){
+	            count++;
+	        }
+	    }
+	    cout<<count<<endl;
+	    
+	}
+	return 0;
+}
