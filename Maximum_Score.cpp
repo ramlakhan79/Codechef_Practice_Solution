4954f896cffd20fa146637023c6afeece095189a
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-12-20 21:39:49 +0530

    Create Maximum_Score.cpp

---
+++ Maximum_Score.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    int count=0;
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	        if(arr[i]==0){
+	            count++;
+	        }
+	    }
+	    if(count<=(n-count)){
+	        cout<<count<<endl;
+	    }
+	    else{
+	        cout<<n-count<<endl;
+	    }
+	   
+	   
+	    
+	}
+	return 0;
+}
