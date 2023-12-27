8109fa29d1da31191dae6d4a567feec35ec368c9
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-12-27 23:10:20 +0530

    Create Christmas_Candy.cpp

---
+++ Christmas_Candy.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    int mx=0;
+	    int cd=0;
+	    vector<int>v;
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    for(int i=0;i<n;i++){
+	        mx=max(mx,arr[i]);
+	        v.push_back(mx);
+	        if(v[i]>arr[i]){
+                cd++;
+            }
+	    }
+        cout <<cd<< endl;
+	}
+}
