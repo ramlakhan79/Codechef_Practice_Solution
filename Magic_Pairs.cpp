ebfbd65f9ebb420c647e2ca771e3aa5eb3bf44a6
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-01-20 21:10:19 +0530

    Create Magic_Pairs.cpp

---
+++ Magic_Pairs.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n;
+	    cin>>n;
+	    int arr[n];
+	    int cnt=0;
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    cout<<((n-1)*(n))/2<<endl;
+	}
+
+}
