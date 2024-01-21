342d53ecd286ef262f9447ffd0b5b8a5c38793ce
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-01-21 22:52:18 +0530

    Create Coins_and_Tringle.cpp

---
+++ Coins_and_Tringle.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int res=0,k=1;
+	    while(n>0){
+	        if(n>=k){
+	            res++;
+	        }
+	        n-=k;
+	        k++;
+	    }
+	    cout<<res<<endl;
+	}
+	return 0;
+
+}
