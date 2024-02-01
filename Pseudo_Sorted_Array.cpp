8d8b16995c8a1a4754a469a068c0693f0d8be185
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-02-01 16:09:13 +0530

    Create Pseudo_Sorted_Array.cpp

---
+++ Pseudo_Sorted_Array.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    bool flag=true;
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    for(int i=1;i<n;i++){
+	        if(arr[i]>=arr[i-1]){
+	            continue;
+	        }    
+	        else{
+	            int temp=arr[i];
+	            arr[i]=arr[i-1];
+	            arr[i-1]=temp;
+	            break;
+	        }
+	    }
+	    for(int i=1;i<n;i++){
+	        if(arr[i]<arr[i-1]){
+	            flag=false;
+	        }    
+	        
+	    }
+	    if(flag){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
