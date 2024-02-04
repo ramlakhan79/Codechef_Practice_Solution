0d5d10acceef0a237327538d1f27bf784be7d839
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-02-04 11:54:15 +0530

    Create String_protocol.cpp

---
+++ String_protocol.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int res=0;
+        char ch;
+        for(int i=1;i<n;i++){
+            if(s[i-1]==s[i]){
+                ch=s[i];
+                i+=1;
+                res++;
+                cnt++;
+            }
+            else if(ch==s[i]){
+                continue;
+                
+            }
+        }
+        cout<<n-(res)<<endl;
+    }
+}
