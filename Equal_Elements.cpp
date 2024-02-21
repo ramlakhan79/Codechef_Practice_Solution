3c12002d93860b53a415cac7092b7da77007386c
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-02-21 22:26:26 +0530

    Create Equal_Elements.cpp

---
+++ Equal_Elements.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        map<int, int>mp;
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+        int max=0;
+        for(auto it : mp){
+            if(max<it.second)
+                max=it.second;
+        }
+        cout<<n-max<<endl;
+    }
+}
