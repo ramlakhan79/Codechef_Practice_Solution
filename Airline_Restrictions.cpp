4aed14957339beebdc2ff9924d8389dee8c285cb
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-03-07 22:22:22 +0530

    Airline_Restrictions.cpp

--- Airline_Restrictions.cpp
+++ Airline_Restrictions.cpp
@@ -6,8 +6,7 @@ int main() {
 	int t;
 	cin>>t;
 	while(t--){
-	    int a,b,c,d,e;
-		
+	    int a,b,c,d,e;		
 	    cin>>a>>b>>c>>d>>e;
 	    
 	    if((a+b<=d && c<=e) || (b+c<=d && a<=e) || (a+c<=d && b<=e) ){
