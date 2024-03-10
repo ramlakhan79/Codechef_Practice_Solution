fab24bc0676d7e538789de0dcb99ff335ffdfce1
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-03-10 17:46:00 +0530

    is_it_a_V_or_C.cpp

--- is_it_a_V_or_C.cpp
+++ is_it_a_V_or_C.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main() {
 	// your code goes here
 	char ch;
-	cin>>ch;
-	
+	cin>>ch;	
 	if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
 	    cout<<"Vowel"<<endl;		
 	else
