90a0848ad0c933e2948efc16897f53e0b4fd4f87
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-03-27 22:19:21 +0530

    Create Extreme_Basketball.cpp

---
+++ Extreme_Basketball.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define cn(a,b) cin>>a>>b;
+using namespace std;
+
+
+int main() {
+	// your code goes here
+	int t;
+    cin>>t;
+    while(t--){
+        int A, B;
+        cn(A,B);
+        int res=0;
+        int targetScore=B+10; 
+
+        while(A<targetScore){
+            if((targetScore-A)%3==0){
+                res+=(targetScore-A)/3;
+                break;
+            } 
+            else{
+                res++;
+                A+=2;
+            }
+        }
+        cout<<res<<endl;
+    }
+
+}
