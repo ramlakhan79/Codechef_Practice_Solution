cb3e6675ad282542d81f8ed11871fe67eca9ea96
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-03-31 22:24:36 +0530

    Chef_and_his_Apps.cpp

--- Chef_and_his_Apps.cpp
+++ Chef_and_his_Apps.cpp
@@ -1,5 +1,6 @@
 /*  Chef's phone has a total storage of S MB. Also, Chef has 2 apps already installed on his phone which occupy X MB and 
-Y MB respectively.He wants to install another app on his phone whose memory requirement is Z MB. For this, he might have to delete the apps already installed on his phone. Determine the minimum number of apps he has to delete from his phone so that he has enough memory to install the third app.  */
+Y MB respectively.He wants to install another app on his phone whose memory requirement is Z MB. For this, he might have to delete the apps already installed on his phone. Determine the minimum number of apps he has to delete from his phone so that he has enough memory to install the third app.  
+*/
 
 #include <iostream>
 using namespace std;
