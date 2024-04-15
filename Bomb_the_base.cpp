f914aed978d976a3346cba9ae16fd76372d2bc12
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-04-15 23:18:57 +0530

    Create Bomb_the_base.cpp

---
+++ Bomb_the_base.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    int maximum=0;
+	    for(int i=n-1;i>=0;i--){
+	        if(arr[i]<x){
+	            maximum=i+1;
+	            break;
+	        }
+	    }
+	    cout<<maximum<<endl;
+	}
+
+}
