45c7eda63d8a3e8e487535ed67e733c83358e69f
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-04-23 18:38:00 +0530

    Area_Peri.cpp

--- Area_Peri.cpp
+++ Area_Peri.cpp
@@ -5,6 +5,7 @@ int main() {
 	// your code goes here
 	int l,b;
 	cin>>l>>b;	
+	
 	if(l*b>(2*(l+b))) cout<<"Area"<<endl<<l*b; 
  	else if(l*b<(2*(l+b)))cout<<"Peri"<<endl<<2*(l+b);
 	else cout<<"Eq"<<endl<<l*b;
