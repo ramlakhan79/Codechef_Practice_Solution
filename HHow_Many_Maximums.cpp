c3da4f92af65117571a9e78d1886becc2d04fa92
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-04-28 16:19:45 +0530

    Create HHow_Many_Maximums.cpp

---
+++ HHow_Many_Maximums.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n, one=0;
+	    string s="";
+	    bool k = true;
+	    cin>>n;
+	    cin>>s;
+	    for(int i=0; i<n-1; i++){
+	        if(s[i]=='1' && k==true){
+	            one++;
+	            k=false;
+	        }
+	        else if(s[i]=='0'){
+	            k=true;
+	        }
+	    }
+	    if(k) cout<<one+1<<endl;
+	    else cout<<one<<endl;
+	}
+
+}
