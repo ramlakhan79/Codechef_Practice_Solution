c9e9e65e6e0704bd8db58264bfcd628bb6332a90
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-05-02 22:59:48 +0530

    Create Change_Please.cpp

---
+++ Change_Please.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int c=100-x;
+	    int res=(c/10)*10;
+	    cout<<res<<endl;
+	}
+	return 0;
+
+}
