538f5a35f489be6f8717644bff916035968c898b
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-05-14 23:16:23 +0530

    Far_Away.cpp

--- Far_Away.cpp
+++ Far_Away.cpp
@@ -17,15 +17,7 @@ int main() {
 	   long ans=0;
 	   long max=0;
 	    for(int i=0;i<n;i++){
-	       // int x=0;
-	       // if(arr[i]<m){
-	       //     x=t;
-	       //     m=m-arr[i];
-	       // }
-	       // else{
-	       //     x=1;
-	       // }
-	       // ans+=abs(arr[i]-x);
+	      
 	       if(arr[i]>m/2){
 	            max=abs(arr[i]-1);
 	        }
