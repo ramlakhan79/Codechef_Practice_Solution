c630d551fcf5ce77b8dee556dae8f4f80057b400
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2024-06-10 23:43:26 +0530

    Create Program_Your_Own_CALCULATOR.cpp

---
+++ Program_Your_Own_CALCULATOR.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	double a,b;
+	char c;
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	switch(c){
+	    case '+':cout<<(a+b)<<endl;
+	        break;
+	    case '-':cout<<(a-b)<<endl;
+	        break;
+	    case '*':cout<<(a*b)<<endl;
+	        break;
+	    case '/':
+	        if(b!=0){
+                cout<<fixed<<setprecision(7)<<(a/b)<<endl;
+            }
+            else{
+                cout<<fixed<<setprecision(7)<<"0"<<endl;
+            }
+	        break;        
+	        
+	}
+	return 0;
+
+}
